ae8414c7423394ebebee0dd62bf630b489b37db0
Yakup <yakupjoraev@gmail.com>
2023-06-25 02:12:46 +0300

    added new sections

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -53,3 +53,72 @@ function fixedNav() {
 }
 window.addEventListener('scroll', fixedNav)
 
+
+function casesSlider() {
+  var swiper = new Swiper(".cases__slider", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    navigation: {
+      nextEl: ".cases__slider-next",
+      prevEl: ".cases__slider-prev",
+    },
+
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1.2,
+        spaceBetween: 16
+      },
+      // when window width is >= 480px
+      767: {
+        slidesPerView: 2,
+        spaceBetween: 16
+      },
+      // when window width is >= 640px
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 16,
+      }
+    }
+  });
+}
+
+casesSlider();
+
+function tabs(headerSelector, tabSelector, contentSelector, activeClass, display = 'flex') {
+  const header = document.querySelector(headerSelector),
+    tab = document.querySelectorAll(tabSelector),
+    content = document.querySelectorAll(contentSelector)
+  function hideTabContent() {
+    content.forEach(item => {
+      item.style.display = 'none'
+    });
+    tab.forEach(item => {
+      item.classList.remove(activeClass)
+    });
+  }
+  function showTabContent(i = 0) {
+    content[i].style.display = display
+    tab[i].classList.add(activeClass)
+  }
+  hideTabContent()
+  showTabContent()
+  header.addEventListener('click', e => {
+    const target = e.target
+    if (target.classList.contains(tabSelector.replace(/\./, '')) ||
+      target.parentNode.classList.contains(tabSelector.replace(/\./, ''))) {
+      tab.forEach((item, i) => {
+        if (target == item || target.parentNode == item) {
+          hideTabContent()
+          showTabContent(i)
+        }
+      });
+    }
+  })
+}
+
+// ПЕРВЫЙ аргумент - класс всего нашего хедера табов.
+// ВТОРОЙ аргумент - класс конкретного элемента, при клике на который будет переключатся таб.
+// ТРЕТИЙ аргумент - класс того блока, который будет переключаться.
+// ЧЕТВЕРТЫЙ аргумент - класс активности, который будет добавлятся для таба, который сейчас активен.
+tabs('.tabs__header', '.tabs__header-item', '.tabs__content-item', 'active')
